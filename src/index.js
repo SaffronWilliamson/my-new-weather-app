8fb6e9d42f8555f606a4e8c9964dada1622c3d9d
SaffronWilliamson <saffron.havana99@gmail.com>
2024-11-15 18:10:52 +0000

    Added changing weather icons

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector('#wind-speed');
   let timeElement = document.querySelector('#time');
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector('#icon');
 
-  descriptionElement.innerHTML = response.data.condition.description;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function formatDate(date) {
